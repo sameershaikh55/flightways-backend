b33f104cbfbe54e174b7fc8f5899ed6ec231a558
sameershaikh55 <sameermubeen4@gmail.com>
2022-08-04 01:52:49 +0500

    updated final

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", function (req, res) {
-//   res.send("IT WORKED");
-// });
-// app.get("/new", function (req, res) {
-//   res.send("New also worked");
-// });
-
-
-
 const express = require("express");
 require("dotenv").config();
 const { Duffel } = require("@duffel/api");

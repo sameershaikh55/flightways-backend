808136512336065c563ca1fed5f3a06b4cafd262
sameershaikh55 <sameermubeen4@gmail.com>
2022-11-10 01:55:39 +0500

    updated

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const { Duffel } = require("@duffel/api");
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 
 const duffel = new Duffel({
   // Store your access token in an environment variable, keep it secret and only readable on your server
